af549522731a8965dcbd8c0023e4d77f1ab89956
Andrew Henrique <andrews-loly@hotmai.com>
2023-03-24 19:52:32 -0300

    primeiro commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import { Container, Profile, Logout } from "./style"
+import { RiShutDownLine} from "react-icons/ri"
+
+
+export function Header(){
+  return (
+    <Container>
+      <Profile to='/profile'>
+        <img src="https://github.com/rodrigorgtic.png" alt="Foto do usuário"></img>
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Rodrigo Gonçalves</strong>    
+        </div>
+      </Profile>
+
+      <Logout >
+        <RiShutDownLine /> 
+      </Logout>
+    </Container>
+  )
+}
